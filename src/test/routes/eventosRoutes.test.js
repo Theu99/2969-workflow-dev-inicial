c188610dd348bd3a0715cd8beb892f4fc4888529
Angelo Reis <matheusreis1441@gmail.com>
2025-04-22 15:12:54 -0300

    feat: :sparkles: add: adicionando a feature flag em /eventos e colocando testes em /eventos

--- src/test/routes/eventosRoutes.test.js
+++ src/test/routes/eventosRoutes.test.js
@@ -12,7 +12,13 @@ after(async () => {
 });
 
 describe('GET em /eventos', () => {
+
+    afterEach(() => {
+        delete process.env.EVENTO_FLAG;
+    });
+
     it('Deve retornar uma lista de eventos', (done) => {
+        process.env.EVENTO_FLAG = 'true'
         chai.request(app)
             .get('/eventos')
             .set('Accept', 'application/json')
@@ -22,6 +28,19 @@ describe('GET em /eventos', () => {
                 expect(res.body[0]).to.have.property('id');
                 expect(res.body[0]).to.have.property('nome');
                 expect(res.body[0]).to.have.property('descricao');
+                done();
+            })
+    });
+
+    it('Deve retornar erro 404', (done) => {
+        process.env.EVENTO_FLAG = 'false'
+        chai.request(app)
+            .get('/eventos')
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                expect(res.status).to.equal(404);
+                expect(res.body).to.have.property('message')
+                    .eql('Você não tem autorização para entrar nessa rota');
                 done();
             })
     })
